50c91fad001579f856de4c3dce1e57b0add802cd
jaykchen@gmail.com <jaykchen@gmail.com>
2023-06-16 22:15:11 -0400

    spin a browser use tabs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use headless_chrome::{
-    protocol::cdp::Target::CreateTarget, types::PrintToPdfOptions, Browser, LaunchOptions,
-};
+use headless_chrome::{types::PrintToPdfOptions, Browser, LaunchOptions, browser::Tab};
 use html2text;
-use http_req::request;
 use pdfium_render::prelude::*;
 use readah::readability::Readability;
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 use std::{fmt, str::FromStr};
 use std::{net::SocketAddr, path::PathBuf};
 use url::Url;
@@ -20,10 +18,8 @@ use url::Url;
 #[tokio::main]
 async fn main() {
     // let addr = SocketAddr::from(([10, 0, 0, 75], 5000));
-    let addr = SocketAddr::from(([10, 0, 0, 15], 3000));
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/api", post(handle_post));
+    let addr = SocketAddr::from(([10, 0, 0, 29], 3000));
+    let app = Router::new().route("/api", post(handle_post));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -31,12 +27,17 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Data {
-    url: String,
-}
-
 async fn handle_post(data: Json<Data>) -> impl IntoResponse {
+    let options = LaunchOptions {
+        headless: true,
+        window_size: Some((820, 1180)),
+        path: Some(
+            PathBuf::from_str("/home/jaykchen/projects/scrape-web-by-virtual-printing/chrome/linux-114.0.5735.133/chrome-linux64/chrome").unwrap(),
+        ),
+        ..Default::default()
+    };
+
+    let browser = Browser::new(options).unwrap();
     println!("Received data: {:?}", data.url);
 
     if let Err(_) = Url::from_str(&data.url) {
@@ -45,7 +46,7 @@ async fn handle_post(data: Json<Data>) -> impl IntoResponse {
             .body("parse target url failure".to_string())
             .unwrap();
     } else {
-        match text_to_use(&data.url).await {
+        match text_to_use(&data.url, &browser).await {
             Ok(res) => {
                 return Response::builder()
                     .status(StatusCode::OK)
@@ -62,34 +63,14 @@ async fn handle_post(data: Json<Data>) -> impl IntoResponse {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    url: String,
+}
+
 #[derive(Debug, serde::Serialize)]
 struct MyResponse {
     text: String,
-}
-
-async fn handler(params: Query<Params>) -> Json<MyResponse> {
-    if let Some(url) = &params.url {
-        if let Err(_) = Url::from_str(&url) {
-            return Json(MyResponse {
-                text: "parse target url failure".to_string(),
-            });
-        } else {
-            match get_webpage_text_headless(&url).await {
-                Ok(res) => {
-                    return Json(MyResponse { text: res });
-                }
-                Err(_) => {
-                    return Json(MyResponse {
-                        text: "failed to get text from webpage".to_string(),
-                    })
-                }
-            }
-        }
-    } else {
-        return Json(MyResponse {
-            text: "probably ill-formed request".to_string(),
-        });
-    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -114,37 +95,20 @@ where
     }
 }
 
-async fn get_webpage_text_headless(url: &str) -> anyhow::Result<String> {
-    // set the headless Chrome to open a webpage in portrait mode of certain width and height
-    // here in an iPad resolution, is a way to pursuade webserver to send less non-essential
-    // data, and make the virtual browser to show the central content, for websites
-    // with responsive design, with less clutter
-    let options = LaunchOptions {
-        headless: true,
-        window_size: Some((820, 1180)),
-        path: Some(PathBuf::from_str("/usr/bin/google-chrome").unwrap()),
-        ..Default::default()
-    };
-
-    let browser = Browser::new(options)?;
-
-    let tab = browser.new_tab()?;
-
+async fn get_webpage_text_headless(url: &str, tab: &Tab) -> anyhow::Result<String> {
     tab.navigate_to(url)?;
-    tab.wait_until_navigated()?;
+    tab.wait_for_element_with_custom_timeout("body", Duration::from_secs(7))?;
 
     let pdf_options: Option<PrintToPdfOptions> = Some(PrintToPdfOptions {
         landscape: Some(false),
         display_header_footer: Some(false),
         print_background: Some(false),
-        scale: Some(0.5),
         paper_width: Some(11.0),
         paper_height: Some(17.0),
         margin_top: Some(0.1),
         margin_bottom: Some(0.1),
         margin_left: Some(0.1),
         margin_right: Some(0.1),
-        // page_ranges: Some("1-2".to_string()),
         ignore_invalid_page_ranges: Some(true),
         prefer_css_page_size: Some(false),
         transfer_mode: None,
@@ -154,13 +118,9 @@ async fn get_webpage_text_headless(url: &str) -> anyhow::Result<String> {
     let pdf_data = tab.print_to_pdf(pdf_options)?;
 
     let pdf_as_vec = pdf_data.to_vec();
-    //code below uses dynamically linked libpdfium.dylib on a M1 Mac
-    //it takes some efforts to bind libpdfium on different platforms
-    //please visit https://github.com/ajrcarey/pdfium-render/tree/master
-    //for more details
     let text = Pdfium::new(
         Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
-            "/home/ubuntu/pdfium/lib/",
+            "/home/jaykchen/pdfium/lib/",
             // "/Users/jaykchen/Downloads/pdfium-mac-arm64/lib/libpdfium.dylib",
         ))
         .or_else(|_| Pdfium::bind_to_system_library())?,
@@ -175,20 +135,10 @@ async fn get_webpage_text_headless(url: &str) -> anyhow::Result<String> {
     Ok(text)
 }
 
-pub async fn get_html_headless(url: &str) -> anyhow::Result<String> {
-    let options = LaunchOptions {
-        headless: true,
-        window_size: Some((820, 1180)),
-        path: Some(PathBuf::from_str("/usr/bin/google-chrome").unwrap()),
-        ..Default::default()
-    };
-
-    let browser = Browser::new(options)?;
-    let tab = browser.new_tab()?;
+pub async fn get_html_headless(url: &str, tab: &Tab) -> anyhow::Result<String> {
     tab.navigate_to(url)?;
-    tab.wait_until_navigated()?;
+    tab.wait_for_element_with_custom_timeout("body", Duration::from_secs(7))?;
     let text = tab.get_content()?;
-
     Ok(text)
 }
 
@@ -204,9 +154,11 @@ pub async fn extract_article_text_from_html(url: &str, html_str: String) -> anyh
     Ok(output)
 }
 
-pub async fn text_to_use(url: &str) -> anyhow::Result<String> {
-    let pdf_text = get_webpage_text_headless(url).await?;
-    let html_str = get_html_headless(url).await?;
+pub async fn text_to_use(url: &str, browser: &Browser) -> anyhow::Result<String> {
+    let tab = browser.wait_for_initial_tab().unwrap();
+
+    let pdf_text = get_webpage_text_headless(url, &tab).await?;
+    let html_str = get_html_headless(url, &tab).await?;
     let readah_text = extract_article_text_from_html(url, html_str).await?;
 
     let readah_text_len = readah_text.split_whitespace().count();
